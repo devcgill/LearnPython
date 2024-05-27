d800fd46c38c86096b5cfbd6798ff2512bd63c1f
cg <dev.cgill@gmail.com>
2024-05-27 14:48:59 -0400

    removed old data

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,2 @@
-# Hello World program in Python
-print("This line will be printed.")
-print("This line will be printed again")
-print("new line from py")
-#this is just a comment added in 2024
-print("hello agains")
+print("hello world")
+#just a test program
