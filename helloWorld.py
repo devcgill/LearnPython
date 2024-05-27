9d5b9ecaea1fd926081d902c973b027143018eba
devcgill <83736765+devcgill@users.noreply.github.com>
2024-05-27 12:35:25 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 # Hello World program in Python
 print("This line will be printed.")
+print("This line will be printed again")
 #Following change was made from AWS Linux dev machine
 print("Hello CG")
