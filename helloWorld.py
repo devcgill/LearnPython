fccbd5de0cb519e93e05c319ff98bbd1fb62d9d3
cg <dev.cgill@gmail.com>
2024-05-27 14:27:47 -0400

    changes done from local dev pycharm

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,4 @@
 # Hello World program in Python
 print("This line will be printed.")
 print("This line will be printed again")
-#Following change was made from AWS Linux dev machine
-print("Hello CG")
+print("new line from py")
